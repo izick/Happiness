b8bf748b333f644e6246a3ad6d310caa594dd2d7
izick <izick23@gmail.com>
2012-08-10 12:09:35 -0700

    init

---
+++ Happiness/HappinessAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HappinessAppDelegate.h
+//  Happiness
+//
+//  Created by Tony Scarpino on 8/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Tony Scarpino on 8/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
